b5c3fc447c5282ec2da66b3e26b04a4f8fee3626
pauloparada <paulodavidparada@gmail.com>
2016-01-14 13:43:40 -0500

    chan

--- autenticarse2.php
+++ autenticarse2.php
@@ -7,31 +7,26 @@ $usr = mysqli_real_escape_string($cnx,$_POST['usuario']);
 $pwd = mysqli_real_escape_string($cnx,$_POST['password']);
 // Hago la consulta
 $sql = 'SELECT * FROM usuarios WHERE usuario = "'.$usr.'" AND password = "'.$pwd.'"';
-$uno = 'SELECT nombre FROM usuarios WHERE usuario = "'.$usr.'" AND password = "'.$pwd.'"';
-$dos = 'SELECT apellidos FROM usuarios WHERE usuario = "'.$usr.'" AND password = "'.$pwd.'"';
-$tres = 'SELECT foto FROM usuarios WHERE usuario = "'.$usr.'" AND password = "'.$pwd.'"';
-$cuatro = 'SELECT edad FROM usuarios WHERE usuario = "'.$usr.'" AND password = "'.$pwd.'"';
+
 // EJECUTO LA CONSULTA
 $consulta = mysqli_query($cnx,$sql);
+$result = mysqli_fetch_array($consulta);
 $cantidad_resultados = mysqli_num_rows($consulta);
 if($cantidad_resultados == 1){
 	//LO MANDO A privado.php
-	$_SESSION['logueado'] = true;
-	$datosUsuario = mysqli_fetch_assoc($consulta);
-	$_SESSION['usuario'] = $datosUsuario;
-
-	$_SESSION['nombre'] = $uno;
-	$_SESSION['apellidos'] = $dos;
-	$_SESSION['foto'] = $tres;
-	$_SESSION['edad'] = $cuatro;
-
-
+	//$_SESSION['logueado'] = true;
+	//$datosUsuario = mysqli_fetch_assoc($consulta);
+	//$_SESSION['usuario'] = $datosUsuario;
+	//$_SESSION['user_test'] = 'userTest';
+	
+	/*header("location:index.html?id=".$result[0]."&nombre=".$result[1]."&apellidos=".$result[2]);*/
 	?>
 	<script type="text/javascript">
-		header('Location: index.html');
+		window.history.go(-2);
+		alert('Hubo un error en el logueo. las variables son: Id= '.$result[0].' Nombre='.$result[1].' Apellido='.$result[2].' ');
 	</script>
 	
-	<?php	
+	<?php		
 } else {
 	// LO MANDO A LOGUEARSE INFORMANDOLE DEL ERROR
 	?>
