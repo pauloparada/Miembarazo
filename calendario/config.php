b055406271077542b6b279bcc415064ff5709629
pauloparada <paulodavidparada@gmail.com>
2016-01-17 13:38:42 -0500

    c

--- calendario/config.php
+++ calendario/config.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect('localhost','root','','calendario');
+$con = mysqli_connect('comprandoofertascom.fatcowmysql.com','calendario3','calendario3','calendario3');
 ?>
--- calendario/config_produccion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$con = mysqli_connect('comprandoofertascom.fatcowmysql.com','calendario3','calendario3','calendario3');
-?>
