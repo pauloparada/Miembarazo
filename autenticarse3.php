0853808e75b81f8b116affc53de98b86cd8da50b
pauloparada <paulodavidparada@gmail.com>
2016-01-26 14:11:23 -0500

    cc

---
+++ autenticarse3.php
@@ -0,0 +1,42 @@
+<?php	 	
+session_start();
+require('conexion.php');
+$cnx = conectarse();
+//Sanitizo los campos
+$usr = mysqli_real_escape_string($cnx,$_POST['usuario']);
+$pwd = mysqli_real_escape_string($cnx,$_POST['password']);
+// Hago la consulta
+$sql = 'SELECT * FROM usuarios WHERE usuario = "'.$usr.'" AND password = "'.$pwd.'"';
+
+// EJECUTO LA CONSULTA
+$consulta = mysqli_query($cnx,$sql);
+$result = mysqli_fetch_array($consulta);
+$cantidad_resultados = mysqli_num_rows($consulta);
+$data[]=$result;
+if($cantidad_resultados == 1){
+	//LO MANDO A privado.php
+	//$_SESSION['logueado'] = true;
+	//$datosUsuario = mysqli_fetch_assoc($consulta);
+	//$_SESSION['usuario'] = $datosUsuario;
+	//$_SESSION['user_test'] = 'userTest';
+	
+	/*header("location:index.html?id=".$result[0]."&nombre=".$result[1]."&apellidos=".$result[2]);*/
+
+echo json_encode($data);
+	?>
+	<script type="text/javascript">
+	
+
+	</script>
+	
+	<?php		
+} else {
+	// LO MANDO A LOGUEARSE INFORMANDOLE DEL ERROR
+	?>
+	<script type="text/javascript">
+		alert('Hubo un error en el logueo');
+		window.history.go(-1);
+	</script>
+	
+	<?php	 	
+}
